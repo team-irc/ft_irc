dddce9ab444291f6fdcad507b89f25a5f14020b7
Jinwoo Kim <jinwkim@c2r2s3.42seoul.kr>
2021-03-29 11:27:18 +0900

    Add Squit message

--- command/SquitCommand.cpp
+++ command/SquitCommand.cpp
@@ -4,20 +4,30 @@
 void	SquitCommand::run(IrcServer &irc)
 {
 	Socket	*sock = irc.get_current_socket();
+	unsigned short		port;
+	int					fd = irc.get_current_socket()->get_fd();
+	std::string			msg = "";
 
 	// SQUIT은 서버에서 보내는 메시지라서 다른 타입이면 걍 무시
 	if (sock->get_type() == SERVER)
 	{
 		if (_msg.get_prefix().empty())
 		{
+			port = irc.get_current_socket()->get_port();
 			// 이 경우는 직접 연결된 서버에서 연결 종료 메시지를 보낸 경우
 			// fd_map과 socketset에서 둘 다 지워야 함
+			irc.delete_fd_map(port);
+			irc.get_socket_set().remove_socket(irc.get_current_socket());
+			msg += ":" + std::to_string(port) + " " + _msg.get_msg();
 		}
 		else
 		{
 			// 이 경우는 그냥 전달받은 경우
 			// fd_map에서 해당 포트 번호에 해당하는 값을 지우면 됨
+			port = ft::atoi(_msg.get_prefix().c_str());
+			irc.delete_fd_map(port);
 		}
+		irc.send_msg_server(fd, msg.c_str());
 	}
 }
 
