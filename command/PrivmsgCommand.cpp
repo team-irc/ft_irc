7bedadb8dbbd681fd28ca70e1c843356ba9ba924
jinwkim <jinwoo700@gmail.com>
2021-03-31 17:02:39 +0900

    Fix Channel member vector to struct vector
    replace Member pointer -> ChanMember

--- command/PrivmsgCommand.cpp
+++ command/PrivmsgCommand.cpp
@@ -39,9 +39,9 @@ void			PrivmsgCommand::send_member(IrcServer &irc, Member &member)
 
 void			PrivmsgCommand::send_channel(IrcServer &irc, Channel &channel)
 {
-	std::vector<Member *>	members = channel.get_members();
-	std::vector<Member *>::iterator begin = members.begin();
-	std::vector<Member *>::iterator end = members.end();
+	std::vector<ChanMember>	members = channel.get_members();
+	std::vector<ChanMember>::iterator begin = members.begin();
+	std::vector<ChanMember>::iterator end = members.end();
 	int			fd;
 	std::string	prefix;
 
@@ -49,11 +49,11 @@ void			PrivmsgCommand::send_channel(IrcServer &irc, Channel &channel)
 	prefix = _msg.get_prefix().substr(0, _msg.get_prefix().find('!'));
 	while (begin != end)
 	{
-		fd = (*begin)->get_fd();
+		fd = (*begin)._member->get_fd();
 		// 해당 메시지를 보낸 유저에는 메시지를 전송하지 않아야 함
 		// 채널 내에 있을 수도 있고 없을 수도 있음(prefix로 구분?)
-		if (((*begin)->get_nick() != prefix) &&
-			((*begin)->get_fd() != _msg.get_source_fd()))
+		if (((*begin)._member->get_nick() != prefix) &&
+			((*begin)._member->get_fd() != _msg.get_source_fd()))
 			(irc.get_socket_set().find_socket(fd))->write(_msg.get_msg());
 		begin++;
 	}
