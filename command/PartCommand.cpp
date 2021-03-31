f588f959288169fbd364c31fb6ae62f5c3610fc2
Hangyeol Na <ghnruf@naver.com>
2021-03-31 12:58:07 +0900

    Fix | Part 명령 수정

--- command/PartCommand.cpp
+++ command/PartCommand.cpp
@@ -17,44 +17,41 @@ void	PartCommand::run(IrcServer &irc)
 	if (socket->get_type() == SERVER)
 	{
 		size = ft::split(_msg.get_param(0), ',', channel_names);
-		member = irc.find_member(socket->get_fd());
+		std::string	nickname = _msg.get_prefix();
+		member = irc.get_member(nickname);
 		for (int i = 0; i < size; i++)
 		{
 			channel = irc.get_channel(channel_names[i]);
 			if (channel == NULL)
 				throw (Reply(ERR::NOSUCHCHANNEL(), channel_names[i]));
-			if (!channel->find_member(irc.find_member(socket->get_fd())))
+			if (!channel->find_member(member))
 				throw (Reply(ERR::NOTONCHANNEL(), channel_names[i]));
-			// 	channel->delete_member(member);
+			channel->delete_member(member);
+			member->delete_channel(channel);
 		}
 		irc.send_msg_server(socket->get_fd(), _msg.get_msg()); 
 	}
 	else if (socket->get_type() == CLIENT) // 클라이언트에서 온 경우
 	{
-		_msg.set_prefix(member->get_nick());
-
 		size = ft::split(_msg.get_param(0), ',', channel_names);
 		member = irc.find_member(socket->get_fd());
+		_msg.set_prefix(member->get_nick());
 		for (int i = 0; i < size; i++)
 		{
 			channel = irc.get_channel(channel_names[i]);
 			if (channel == NULL)
 				throw (Reply(ERR::NOSUCHCHANNEL(), channel_names[i]));
-			if (!channel->find_member(irc.find_member(socket->get_fd())))
+			if (!channel->find_member(member))
 				throw (Reply(ERR::NOTONCHANNEL(), channel_names[i]));
-			// 	channel->delete_member(member);
+			channel->delete_member(member);
+			member->delete_channel(channel);
 		}
 		irc.send_msg_server(socket->get_fd(), _msg.get_msg());
-		// 2. 채널목록으로 채널을 가져온다.
-		// 3. 채널의 활성 멤버에서 자기를 제거한다.
-		// 4. 채널에 없다면 에러
-		// 5. 다른서버에도 메세지를 전송
 	}
 	else
 	{
 		return ;
 	}
-	std::cout << "Part Command executed.\n";
 }
 
 PartCommand::PartCommand() : Command()
