dd01c9cf6282be876ac20667a307929ad71a323f
Youngjin Choi <yochoi@c4r9s6.42seoul.kr>
2021-04-06 16:18:45 +0900

    server name testPORT.com 으로 변경

--- ft_irc.cpp
+++ ft_irc.cpp
@@ -16,6 +16,7 @@ IrcServer::IrcServer(int argc, char **argv)
 		_listen_socket->bind();
 		_listen_socket->listen();
 		_fd_map.insert(std::pair<std::string, int>(_si.SERVER_NAME, _listen_socket->get_fd()));
+		_si.SERVER_NAME = std::string("test") + std::to_string(_listen_socket->get_port()) + ".com";
 		_my_pass = std::string(argv[argc == 4 ? 3 : 2]);
 		time(&_start_time);
 	}
@@ -44,7 +45,7 @@ void	 IrcServer::connect_to_server(char **argv)
 	// 이 시점에서 PASS 보내고
 	std::string		msg = "PASS " + new_socket->get_pass() + "\n";
 	new_socket->write(msg.c_str());
-	msg = "SERVER " + _si.SERVER_NAME + " :connect!\n";
+	msg = "SERVER " + _si.SERVER_NAME + " 0 :connect!\n";
 	new_socket->write(msg.c_str());
 
 	// 서버 내부 map에 있는 데이터를 send_msg로 전송해야 함
