45e3594030b9f6ced6736db67d0001c2d688d174
Youngjin Choi <yochoi@c2r9s5.42seoul.kr>
2021-04-16 14:06:51 +0900

    sids

--- srcs/Command.cpp
+++ srcs/Command.cpp
@@ -37,7 +37,6 @@ void Command::execute(IrcServer &irc)
 	try
 	{
 		run(irc);
-		Member * member = irc.find_member(irc.get_current_socket()->get_fd());
 		std::cout << _msg.get_command() << " Command executed" << std::endl;
 	}
 	catch (const Reply &rpl)
