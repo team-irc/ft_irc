45480cd722ed851604e4cf7c1b75b58f52707a0c
Youngjin Choi <yochoi@c4r10s6.42seoul.kr>
2021-04-20 14:09:50 +0900

    QuitCommand 사용 시 채널 멤버에게 알리는 기능 추가

--- srcs/command/QuitCommand.cpp
+++ srcs/command/QuitCommand.cpp
@@ -66,6 +66,26 @@ static void		quit(IrcServer &irc, Member *member)
 	}
 }
 
+static void		notice_to_channel_member(Member * member, std::string quit_message)
+{
+	std::set<Channel *>::iterator	first;
+	std::set<Channel *>::iterator	last;
+	std::string						msg;
+
+	first = member->get_joined_channels().begin();
+	last = member->get_joined_channels().end();
+	if (quit_message[0] == ':')
+		quit_message.erase(0, 1);
+	while (first != last)
+	{
+		msg = ':' + member->get_nick() + "!~" + member->get_username() + "@" + member->get_hostname() + " QUIT :";
+		msg += quit_message + '\n';
+		(*first)->send_msg_to_members(msg.c_str());
+		msg.clear();
+		++first;
+	}
+}
+
 void	QuitCommand::run(IrcServer &irc)
 {
 	Socket		*socket;
@@ -75,6 +95,8 @@ void	QuitCommand::run(IrcServer &irc)
 	if (socket->get_type() == CLIENT)
 	{
 		member = irc.find_member(socket->get_fd()); // 1. 멤버를 찾는다.
+		if (!member->get_joined_channels().empty())
+			notice_to_channel_member(member, _msg.get_param_size() >= 1 ? _msg.get_param(0) : member->get_nick());
 		if (member)
 			_msg.set_prefix(member->get_nick()); 	// 2. 메세지를 전파하기 위해 닉네임을 프리픽스로 설정
 		quit(irc, member);							// 3. 제거
@@ -82,21 +104,21 @@ void	QuitCommand::run(IrcServer &irc)
 	}
 	else if (socket->get_type() == SERVER)
 	{
-		member = irc.get_member(_msg.get_prefix()); // 1. prefix로 멤버를 찾는다.
-		quit(irc, member); // 2. 멤버 제거
-		irc.send_msg_server(socket->get_fd(), _msg.get_msg()); // 3. 다른서버에도 메세지를 보낸다.
+		member = irc.get_member(_msg.get_prefix());				// 1. prefix로 멤버를 찾는다.
+		quit(irc, member);										// 2. 멤버 제거
+		irc.send_msg_server(socket->get_fd(), _msg.get_msg());	// 3. 다른서버에도 메세지를 보낸다.
 	}
 	else if (socket->get_type() == UNKNOWN)
 	{
-		member = irc.find_member(socket->get_fd()); // 1. fd값으로 멤버를 찾는다.
+		member = irc.find_member(socket->get_fd());	// 1. fd값으로 멤버를 찾는다.
 		if (member)
 		{
-			irc.delete_member(member->get_nick()); // 2. 멤버를 _global_user에서 지운다.
+			irc.delete_member(member->get_nick());	// 2. 멤버를 _global_user에서 지운다.
 			quit_from_joined_channel(irc, member);
-			delete member; //멤버 인스턴스를 제거
+			delete member; 							//멤버 인스턴스를 제거
 		}
-		irc.get_socket_set().remove_socket(socket); // socket_set에서 제거
-		delete socket; // 3. 소켓 연결을 끊는다. 다른 서버에는 전송하지 않는다. 다른서버에는 이 멤버가 등록되지 않은 상태이므로
+		irc.get_socket_set().remove_socket(socket);	// socket_set에서 제거
+		delete socket; 								// 3. 소켓 연결을 끊는다. 다른 서버에는 전송하지 않는다. 다른서버에는 이 멤버가 등록되지 않은 상태이므로
 	}
 }
 
