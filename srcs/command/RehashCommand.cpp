c1deddc8339efcdd2bc423a7d583cca86ae4a81e
Youngjin Choi <yochoi@c4r10s6.42seoul.kr>
2021-04-20 13:26:34 +0900

    fix rehash

--- srcs/command/RehashCommand.cpp
+++ srcs/command/RehashCommand.cpp
@@ -11,11 +11,7 @@ void    RehashCommand::run(IrcServer & irc)
 	rc.open_file("ft_irc.conf");
 	rc.read_config(irc.get_serverinfo());
 	if (irc.get_serverinfo().SERVER_NAME == "${AUTO}")
-<<<<<<< HEAD
 		irc.get_serverinfo().SERVER_NAME = "test" + ft::itos(irc.get_listen_socket()->get_port()) + ".com";
-=======
-		irc.get_serverinfo().SERVER_NAME = std::string("test") + ft::itos(irc.get_listen_socket()->get_port()) + ".com";
->>>>>>> d506fb6f2a87b60f7114380e12574ada479618f0
 	socket->write(Reply(RPL::REHASHING(), "ft_irc.conf").get_msg().c_str());
 }
 
