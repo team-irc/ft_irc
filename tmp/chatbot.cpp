b434fab418c88b4d8bb06dc05f31fe00028cd9b1
Youngjin Choi <yochoi@c10r6s12.42seoul.kr>
2021-04-26 14:15:30 +0900

    making chatbot

--- tmp/chatbot.cpp
+++ tmp/chatbot.cpp
@@ -7,6 +7,16 @@
 #include <stdio.h>
 #include <algorithm>
 
+const std::string command_list[] = { "ADMIN", "AWAY", "CONNECT", "ERROR", "INFO", "INVITE", "ISON", "JOIN", "KICK", "KILL", "LINKS", "LIST", \
+									"MODE", "NAMES", "NICK", "NOTICE", "OPER", "PART", "PASS", "PING", "PONG", "PRIVMSG", "QUIT", "REHASH", \
+									"SERVER", "SQUIT", "STATS", "TIME", "TOPIC", "TRACE", "USER", "USERHOST", "VERSION", "WHOIS", "WHO", "WHOWAS", ""};
+
+void error(std::string err)
+{
+    std::cout << err << std::endl;
+    exit(1);
+}
+
 int split(const std::string str, char c, std::string *& ret)
 {
 	int size;
@@ -42,53 +52,31 @@ int split(const std::string str, char c, std::string *& ret)
 
 void ignore_motd(int sock)
 {
-	char buf[512];
-	while (1)
+	char		buf[512] = {0, };
+	std::string	tmp;
+	std::string	*split_ret;
+
+	do
 	{
 		read(sock, buf, 512);
-		std::string *ret;
-		split(std::string(buf), ' ', ret);
-		if (ret[3] == ":End")
-		{
-			delete[] ret;
-			break ;
-		}
-		delete[] ret;
-	}
-}
-
-std::string get_message_type(char *message)
-{
-	std::string	ret;
-	int			i = (message[0] == ':' ? 1 : 0);
-
-	while (!std::isupper(message[i]) && message[i] && message[i] != ':')
-		++i;
-	if (message[i] == ':')
-		return (std::string());
-	while (message[i] != ' ')
-		ret += message[i++];
-	return (ret);
+		split(std::string(buf), ' ', split_ret);
+		tmp = split_ret[1];
+		delete[] split_ret;
+	} while (tmp != "376");
+	std::cout << "end of ignore_motd\n";
 }
 
 std::string get_command(char *message)
 {
-	std::string	tmp(message);
 	std::string	ret;
-	std::string::reverse_iterator first;
-	std::string::reverse_iterator last;
+	std::string *split_ret;
 
-	first = tmp.rbegin();
-	last = tmp.rend();
-	while (first != last && *first != ' ')
-	{
-		ret += *first;
-		++first;
-	}
-	std::reverse(ret.begin(), ret.end());
-	if (*(ret.begin()) == ':')
+	split(message, ' ', split_ret);
+	ret = split_ret[3];
+	delete[] split_ret;
+	if (ret[0] == ':')
 		ret.erase(ret.begin());
-	while (*(ret.end() - 1) == '\n' || *(ret.end() - 1) == '\r')
+	if (ret[ret.length() - 1] == '\n' || ret[ret.length() - 1] == '\r')
 		ret.erase(ret.end() - 1);
 	return (ret);
 }
@@ -114,6 +102,76 @@ void	hello_command(int sock, std::string target)
 	write(sock, ret.c_str(), ret.length());
 }
 
+void	help_command(int sock, std::string target, std::string message)
+{
+	const std::string privmsg = "privmsg " + target + ":";
+	if (*(message.begin()) == ':')
+		message.erase(message.begin()); // erase first colon
+	if (message.find(':') != std::string::npos)
+	{
+		std::string tmp;
+		std::string *split_ret;
+		split(message, ':', split_ret);
+		std::string command = split_ret[1];
+		delete[] split_ret;
+		if (split(command, ' ', split_ret) == 1)
+		{
+			tmp = privmsg + "---COMMAND_LIST---\n";
+			write(sock, tmp.c_str(), tmp.length());
+			for (int i = 0; !command_list[i].empty(); ++i)
+			{
+				tmp = privmsg + "   " + command_list[i] + '\n';
+				write(sock, tmp.c_str(), tmp.length());
+			}
+			return ;
+		}
+		else
+		{
+			command = split_ret[1];
+			delete[] split_ret;
+			if (command[command.length() - 1] == '\n' || command[command.length() - 1] == '\r')
+				command.erase(command.end() - 1);
+			std::transform(command.begin(), command.end(), command.begin(), ::toupper);
+			if (command == "ADMIN")
+				tmp = privmsg + "USAGE: ADMIN [<server>]\n";
+			if (command == "AWAY")
+				tmp = privmsg + "USAGE: AWAY [message]\n";
+			if (command == "CONNECT")
+				tmp = privmsg + "USAGE: CONNECT <target server> [<port> [<remote server>]]\n";
+			if (command == "ERROR")
+				tmp = privmsg + "USAGE: ERROR <error message>\n";
+			if (command == "INFO")
+				tmp = privmsg + "USAGE: INFO [<server>]\n";
+			if (command == "INVITE")
+				tmp = privmsg + "USAGE: INVITE <nickname> <channel>\n";
+			if (command == "ISON")
+				tmp = privmsg + "USAGE: ISON <nickname>{<space><nickanem>}\n";
+			if (command == "JOIN")
+				tmp = privmsg + "USAGE: JOIN <channel>{,<channel>} [<key>,{<key>}]\n";
+			if (command == "KICK")
+				tmp = privmsg + "USAGE: KICK <channel> <user> [<comment>]\n";
+			if (command == "KILL")
+				tmp = privmsg + "USAGE: KILL <nickname> <comment>\n";
+			if (command == "LINKS")
+				tmp = privmsg + "USAGE: LINKS [[<remote server>] <server mask>]\n";
+			if (command == "LIST")
+				tmp = privmsg + "USAGE: LIST [<channel>,{<channel>} [<server]\n";
+		}
+		write(sock, tmp.c_str(), tmp.length());
+	}
+	else
+	{
+		std::string tmp;
+		tmp = privmsg + "---COMMAND_LIST---\n";
+		write(sock, tmp.c_str(), tmp.length());
+		for (int i = 0; !command_list[i].empty(); ++i)
+		{
+			tmp = privmsg + "   " + command_list[i] + '\n';
+			write(sock, tmp.c_str(), tmp.length());
+		}
+	}
+}
+
 void	unknown_command(int sock, std::string target, std::string command)
 {
 	std::string ret;
@@ -123,17 +181,8 @@ void	unknown_command(int sock, std::string target, std::string command)
 	write(sock, ret.c_str(), ret.length());
 }
 
-void error(std::string err)
+void connect_to_server(int &sock, int argc, char **argv)
 {
-    std::cout << err << std::endl;
-    exit(1);
-}
-
-int main(int argc, char **argv)
-{
-	int sock;
-	char message[512];
-	int str_len;
 	struct sockaddr_in serv_adr;
 
 	if (argc != 3 && argc != 4)
@@ -148,36 +197,60 @@ int main(int argc, char **argv)
 
 	if (connect(sock, (struct sockaddr *)&serv_adr, sizeof(serv_adr)) == -1)
 		error("connect error");
-	std::cout << "connect" << std::endl;
+}
+
+void send_user_info(int &sock, int argc, char **argv)
+{
 	if (argc == 4)
 	{
 		std::string pass;
-		std::string arg(argv[3]);
-		pass = "pass " + arg + "\n";
+		pass = "pass " + std::string(argv[3]) + "\n";
     	write(sock, pass.c_str(), pass.length());
+		sleep(1);
 	}
     write(sock, "nick bot\n", 9);
+	sleep(1);
     write(sock, "user b b b b\n", 13);
-	ignore_motd(sock);
-	std::cout << "ignore complete\n";
-    while (1)
+}
+
+void run(int &sock)
+{
+	char message[512];
+	std::string	*split_ret;
+
+	std::cout << "run" << std::endl;
+	while (1)
     {
 		memset(message, 0, 512);
         read(sock, message, 512);
+		split(std::string(message), ' ', split_ret);
 		std::cout << "[RECV] " << message;
-		std::string message_type = get_message_type(message); // PRIVMSG
+		std::string message_type = split_ret[1]; // PRIVMSG
 		if (message_type == "PRIVMSG")
 		{
 			std::string command = get_command(message); // /help
-			std::string target = get_target(message); // yochoi	
+			std::string target = get_target(message); // yochoi
 			if (command == "/hello")
 				hello_command(sock, target);
+			if (command == "/help")
+				help_command(sock, target, message);
 			else
 				unknown_command(sock, target, command);
 		}
 		if (message_type == "PING")
 			write(sock, "notice bot :i am alive\n", 23);
+		delete[] split_ret;
     }
+}
+
+int main(int argc, char **argv)
+{
+	int sock;
+
+	connect_to_server(sock, argc, argv);
+	send_user_info(sock, argc, argv);
+	ignore_motd(sock);
+    run(sock);
 	close(sock);
 	return (0);
 }
