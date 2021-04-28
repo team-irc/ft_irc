8852cac3471bec5f79ed9bd9f3114bf04f304fdf
Youngjin Choi <yochoi@c8r5s2.42seoul.kr>
2021-04-28 12:04:54 +0900

    add SIGINT handler

--- srcs/main.cpp
+++ srcs/main.cpp
@@ -3,8 +3,16 @@
 std::string Reply::_user_name = "";
 std::string Reply::_server_name = "";
 
+static void sigint_handler(int _)
+{
+	(void)_;
+	std::cout << "\b \b\b \bSIGINT" << std::endl;
+	exit(0);
+}
+
 int main(int argc, char **argv)
 {
+	signal(SIGINT, sigint_handler);
 	try
 	{
 		IrcServer server(argc, argv);
